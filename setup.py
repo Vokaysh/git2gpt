d1257130e6610e9e49c39d60b68383589a9b854b
Karnagraha <karnagraha@protonmail.com>
2023-03-20 14:31:39 +0000

    git2gpt: Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='git2gpt',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'openai>=0.27.2',
